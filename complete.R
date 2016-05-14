6e8914492bb58793fce68bdcfcbb475843474e1e
LeviWyattP <leviwyattp@gmail.com>
2016-05-14 17:53:53 -0400

    Created complete.R functions

---
+++ complete.R
@@ -0,0 +1,39 @@
+complete <- function(directory, id = 1:332) {
+  
+  
+  #first we are going to load the data
+  df <- data.frame()
+  print(id)
+  
+  # for each id
+  for (i in id)
+  {
+    
+    print(i)
+    # form filename - then add padding 0's
+    file_name <- as.character(i)
+    while (nchar(file_name)<3)
+    {
+      file_name <- paste("0", file_name, sep="")
+    }
+    
+    #combine for full filename
+    filename <- paste(directory, "\\",file_name, ".csv", sep = "")
+    
+    
+    #load csv file
+    csv_file <- read.csv(filename, header = TRUE, na.strings = c("NA","NaN", " "))
+    
+    #remove bad rows
+    csv_file <- na.omit(csv_file)
+    
+      
+    #add the data to the combined data
+    df <- rbind(df, data.frame(id = i, nobs = nrow(csv_file)))
+    
+    
+  }
+  
+  return(df)
+  
+}
